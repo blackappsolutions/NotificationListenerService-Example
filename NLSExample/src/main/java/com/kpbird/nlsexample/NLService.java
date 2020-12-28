22803eae82dd1ed57c87cbff842c8715f97979a3
Markus Schwarz <mschwarz@black-app-solutions.com>
2020-12-28 14:59:42 +0100

    PreferenceActivity to set an IP address of an external display was added.

--- NLSExample/src/main/java/com/kpbird/nlsexample/NLService.java
+++ NLSExample/src/main/java/com/kpbird/nlsexample/NLService.java
@@ -1,78 +1,71 @@
 package com.kpbird.nlsexample;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class NLService extends NotificationListenerService {
 
-    private String TAG = this.getClass().getSimpleName();
-    private NLServiceReceiver nlservicereciver;
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        nlservicereciver = new NLServiceReceiver();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction("com.kpbird.nlsexample.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
-        registerReceiver(nlservicereciver,filter);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(nlservicereciver);
-    }
+    // public static final String TWITTER = "com.twitter.android";
+    public static final String WHATSAPP = "com.whatsapp";
+    private final String TAG = this.getClass().getSimpleName();
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
+        Log.i(TAG, "**********  onNotificationPosted");
+        Log.i(TAG, "ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
 
-        Log.i(TAG,"**********  onNotificationPosted");
-        Log.i(TAG,"ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
-        Intent i = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-        i.putExtra("notification_event","onNotificationPosted :" + sbn.getPackageName() + "\n");
-        sendBroadcast(i);
+        if (WHATSAPP.equals(sbn.getPackageName())) {
+            sendMessageToPixblock("Handy-Nachricht");
+        }
+    }
 
+    private void sendMessageToPixblock(final String msg) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Log.i(TAG, "Response: " + response);
+            }
+        };
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "Error: " + error);
+            }
+        };
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        String ip = sharedPrefs.getString("displayIP", null);
+        String url = "http://" + ip + "/sendToPixBlock";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, responseListener, errorListener) {
+            protected Map<String, String> getParams() {
+                Map<String, String> MyData = new HashMap<String, String>();
+                MyData.put("message", msg);
+                return MyData;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
-        Log.i(TAG,"********** onNOtificationRemoved");
-        Log.i(TAG,"ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText +"\t" + sbn.getPackageName());
-        Intent i = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-        i.putExtra("notification_event","onNotificationRemoved :" + sbn.getPackageName() + "\n");
-
-        sendBroadcast(i);
-    }
-
-    class NLServiceReceiver extends BroadcastReceiver{
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if(intent.getStringExtra("command").equals("clearall")){
-                    NLService.this.cancelAllNotifications();
-            }
-            else if(intent.getStringExtra("command").equals("list")){
-                Intent i1 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-                i1.putExtra("notification_event","=====================");
-                sendBroadcast(i1);
-                int i=1;
-                for (StatusBarNotification sbn : NLService.this.getActiveNotifications()) {
-                    Intent i2 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-                    i2.putExtra("notification_event",i +" " + sbn.getPackageName() + "\n");
-                    sendBroadcast(i2);
-                    i++;
-                }
-                Intent i3 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-                i3.putExtra("notification_event","===== Notification List ====");
-                sendBroadcast(i3);
-
-            }
-
+        Log.i(TAG, "********** onNotificationRemoved");
+        Log.i(TAG, "ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
+        if (WHATSAPP.equals(sbn.getPackageName())) {
+            sendMessageToPixblock(" ");
         }
     }
-
 }
--- README.md
+++ README.md
@@ -1,27 +1,22 @@
-NotificationListenerService-Example
-===================================
+WhatsApp-NotificationListener and External Display Pusher
+=========================================================
 
-##Introduction
-NotificationListenerService is introduced in Android 4.3 (API 18). It allows an application to receive information about notifications as it creates or removes. NotificationListenerService class is derived from the Service class. It has two abstract methods namely 1. onNotificationPosted 2. onNotificationRemoved.  
+This is a fork of https://github.com/kpbird/NotificationListenerService-Example which has the
+following modifications/additions:
+
+* Ability to send/view/delete own notifications were removed.
+* PreferenceActivity to set an IP address of an external display was added.
+* This display will be accessed with the HTTP-POST-Request `http://IP_ADDRESS//sendToPixBlock` and
+* the the parameter `message`, which holds the text the display should present.
+* Currently this message is hard-coded to "Handy-Nachricht" and sent, when a WhatsApp-Notification
+  was detected.
+* When the WhatsApp-Message was read, the message is reset to an empty string, to blank the display.
+
+## Background-Technology
+
+NotificationListenerService is introduced in Android 4.3 (API 18). It allows an application to receive information about notifications as it creates or removes. NotificationListenerService class is derived from the Service class. It has two abstract methods namely 1. onNotificationPosted 2. onNotificationRemoved.
 To use NotificationListenerService, we need to create a java file which extends NotificationListenerService and implement two callback methods. Both methods have a parameter named "sbn", which is an object of StatusBarNotification class. StatusBarNotification provides necessary information about Notifications.
 NotificationListenerService provides facility to fetch active notifications using getActiveNotifications and also provides a feature to remove notifications using cancelAllNotifications.
 
-##Useful Methods
-1. NotificationListenerService
-	* onNotificationPosted
-	* onNotificationRemoved
-2. StatusBarNotification
-	* getId
-	* getNotification
-	* getPackageName
-	* getPostTime
-	* isClearable
-	* isOngoing
-
-##Note
+## Note
 User require to enable notification permission from "Settings > Security > Notification access".
-
-![Mou icon](http://1.bp.blogspot.com/-7Q9G72-ZLCw/UfirCZP-H_I/AAAAAAAAEOk/aqX_YHs6s6Q/s400/device-2013-07-31-113010.png)
-![Mou icon](http://1.bp.blogspot.com/-h_bFIcDWWp8/UfirCzDrC_I/AAAAAAAAEO0/9_aMH5EM6Dg/s400/device-2013-07-31-113539.png)
-![Mou icon](http://2.bp.blogspot.com/-thl_wNKzILI/UfirDX6NR2I/AAAAAAAAEO4/_o5FWLmkJ2o/s400/device-2013-07-31-113701.png)
-![Mou icon](http://1.bp.blogspot.com/-5KyUJQVOVzE/UfirDhqpFzI/AAAAAAAAEPA/RiZoI9dF--Q/s400/device-2013-07-31-113720.png)
